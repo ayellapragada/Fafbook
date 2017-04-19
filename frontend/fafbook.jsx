b102aa1c96876d3c6b74e91ce25bfcd0391dbbd4
Akshith Yellapragada <ayellapragada@gmail.com>
2017-04-19 09:51:25 -0400

    Add session on front end, can log in with ajax

--- frontend/fafbook.jsx
+++ frontend/fafbook.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
+window.store = configureStore();
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to fafbook!</h1>, root);
