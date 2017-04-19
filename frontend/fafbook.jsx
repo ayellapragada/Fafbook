7165104540471c8ab267384073e009ffa48512a2
Akshith Yellapragada <ayellapragada@gmail.com>
2017-04-19 10:20:34 -0400

    Bootstrap current user from backend onto html

--- frontend/fafbook.jsx
+++ frontend/fafbook.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
 import configureStore from './store/store';
 
-window.store = configureStore();
 document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser:window.currentUser } };
+    store = configureStore(preloadedState);
+  } else {
+    store = configure(store);
+  }
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to fafbook!</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
