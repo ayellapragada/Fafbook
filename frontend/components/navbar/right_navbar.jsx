1783fbe2d7a98c9dbeb76b33b815fc4ce39cdcb8
Akshith Yellapragada <ayellapragada@gmail.com>
2017-04-24 22:11:36 -0400

    Making about me intro section

--- frontend/components/navbar/right_navbar.jsx
+++ frontend/components/navbar/right_navbar.jsx
@@ -8,7 +8,7 @@ const RightNavbar = (props) => {
   if (props.currentUser){
     return (
       <div className="right-navbar">
-          <img src={props.currentUser.profile_url}
+        <img src={props.currentUser.profile_url}
           className="navbar-profile-photo"/>
         <Link className="navbar-btn profile-link"
           to={"/profile/" + props.currentUser.id}>
