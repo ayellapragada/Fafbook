02ad90f4726e951bbe450b1c068a235e15b6f841
Akshith Yellapragada <ayellapragada@gmail.com>
2017-04-27 18:33:41 -0400

    On logout clears out state.

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,22 +2,27 @@ import * as APIUtil from '../util/session_api_util'
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+export const USER_LOGOUT = "USER_LOGOUT";
 
 export const signup = user => dispatch => (
   APIUtil.signup(user)
-    .then(user => dispatch(receiveCurrentUser(user)),
-               err => dispatch(receiveErrors(err.responseJSON)))
+  .then(user => dispatch(receiveCurrentUser(user)),
+    err => dispatch(receiveErrors(err.responseJSON)))
 );
 
 export const login = user => dispatch => (
   APIUtil.login(user)
-    .then(user => dispatch(receiveCurrentUser(user)),
-               err => dispatch(receiveErrors(err.responseJSON)))
+  .then(user => dispatch(receiveCurrentUser(user)),
+    err => dispatch(receiveErrors(err.responseJSON)))
 );
 
 export const logout = () => dispatch => (
-  APIUtil.logout().then(user => dispatch(receiveCurrentUser(null)))
-);
+  APIUtil.logout()
+  .then(user =>  {
+    dispatch(receiveCurrentUser(null))
+    dispatch(userLogout())
+  })
+)
 
 export const receiveCurrentUser = currentUser => ({
   type: RECEIVE_CURRENT_USER,
@@ -28,3 +33,7 @@ export const receiveErrors = errors => ({
   type: RECEIVE_ERRORS,
   errors
 });
+
+export const userLogout = () => ({
+  type: USER_LOGOUT
+})
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,15 +1,25 @@
 import { combineReducers } from 'redux';
+import {USER_LOGOUT} from '../actions/session_actions';
 
 import SessionReducer from './session_reducer';
 import UserReducer from './user_reducer';
 import FriendsReducer from './friends_reducer';
 import PostsReducer from './posts_reducer';
 
-const RootReducer = combineReducers({
+
+const AppReducer = combineReducers({
   session: SessionReducer,
   user: UserReducer,
   friends: FriendsReducer,
   posts: PostsReducer,
 })
+
+const RootReducer = (state, action) => {
+  if (action.type === USER_LOGOUT) {
+    state = undefined
+  }
+
+  return AppReducer(state, action);
+}
 
 export default RootReducer;
