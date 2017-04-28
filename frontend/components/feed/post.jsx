505101bf8426527df89dde0f80707748cc538971
Akshith Yellapragada <ayellapragada@gmail.com>
2017-04-28 05:15:09 -0400

    Add buttons to posts for likes and things. Not usable but could do onClick later.

--- frontend/components/feed/post.jsx
+++ frontend/components/feed/post.jsx
@@ -4,6 +4,9 @@ import { Link } from 'react-router';
 class Post extends React.Component {
   constructor(props) {
     super(props);
+    const dateTime = new Date( Date.parse(this.props.post.post.created_at))
+    this.state = {dateTime}
+    debugger
   }
 
 
@@ -23,16 +26,37 @@ class Post extends React.Component {
               <Link to={`/profile/${this.props.post.author.id}`}>
                 {`${this.props.post.author.fname} ${this.props.post.author.lname}`}
               </Link>
+              <div className="post-header-date-time">
+                {this.state.dateTime.toDateString()} at &nbsp;
+                {this.state.dateTime.toLocaleTimeString()}
+              </div>
             </div>
           </div>
 
           <div className="post-body">
+            <p>{this.props.post.post.body}</p>
           </div>
 
+          <div className="post-buttons">
+
+            <div className="post-action-button">
+              <i className="fa fa-thumbs-up" aria-hidden="true"></i>
+              Like
+            </div>
+
+            <div className="post-action-button">
+              <i className="fa fa-comment" aria-hidden="true"></i>
+              Comment
+            </div>
+
+            <div className="post-action-button">
+              <i className="fa fa-share" aria-hidden="true"></i>
+              Share
+            </div>
+
+          </div>
         </div>
 
-        <div className="post-buttons">
-        </div>
 
         <div className="post-likes">
         </div>
