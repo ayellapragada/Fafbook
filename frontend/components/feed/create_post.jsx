cbe4ebc3f3d7f5ac3172768071aecd0ceb678541
Akshith Yellapragada <ayellapragada@gmail.com>
2017-04-27 21:08:56 -0400

    Input form styling done

--- frontend/components/feed/create_post.jsx
+++ frontend/components/feed/create_post.jsx
@@ -6,29 +6,59 @@ class CreatePost extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {input: ""};
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleChange(e) {
-    e.preventDefault();
   }
 
   handleSubmit(e){
     e.preventDefault();
   }
 
+  handleClick(e){
+  }
+
   render() {
+    debugger
     return (
-      <p> This will eventually make a post</p>
+      <div className="create-post">
+
+        <div className="create-post-header-container">
+          <div className="create-post-header">
+            <i className="fa fa-pencil" aria-hidden="true"></i>
+            Create Post!
+          </div>
+        </div>
+
+        <div className="create-post-form-container">
+          <form onSubmit={this.handleSubmit}>
+            <div className="create-post-form-input">
+              <img src={this.props.currentUser.profile_url}/>
+              <textarea
+                value={this.state.input}
+                placeholder="What's on your mind?"
+                onClick={this.handleClick}
+                onChange={this.handleChange} />
+            </div>
+            <div className="create-post-form-submit">
+            </div>
+          </form>
+        </div>
+
+      </div>
     )
   }
 
 }
 
 const mapStateToProps = state => ({
-  errors: state.posts.errors
+  errors: state.posts.errors,
+  currentUser: state.session.currentUser
 })
 
 const mapDispatchToProps = dispatch => ({
