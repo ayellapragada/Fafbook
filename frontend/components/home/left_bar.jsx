8ff67a39d880d7685c5224609169cf488407822f
Akshith Yellapragada <ayellapragada@gmail.com>
2017-05-02 21:40:14 -0400

    Added home left bar styling

--- frontend/components/home/left_bar.jsx
+++ frontend/components/home/left_bar.jsx
@@ -1,28 +1,53 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 class LeftBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return (
-      <div className="inside-home-left-navbar">
-        <div className="home-side-nav-user">
-          Current User Name
-        </div>
+    if (this.props.currentUser) {
+      return (
+        <div className="inside-home-left-navbar">
+          <div className="home-side-nav-user">
+            <Link 
+              to={`/profile/${this.props.currentUser.id}`}>
+              <img src={this.props.currentUser.profile_url}/>
+              {`${this.props.currentUser.fname}
+                ${this.props.currentUser.lname}`}
+            </Link>
+          </div>
 
-        <div className="left-navbar-main">
-          <i className="fa fa-newspaper-o" aria-hidden="true"></i>
-          NewsFeed
-          <br/>
-          <i className="fa fa-comments" aria-hidden="true"></i>
-          Messenger
-        </div>
+          <div className="left-navbar-main">
+            <Link to="/">
+              <i className="fa fa-newspaper-o" aria-hidden="true"></i>
+              <p>News Feed</p>
+            </Link>
+            <Link to="/">
+              <i className="fa fa-comments" aria-hidden="true"></i>
+              <p>Messenger</p>
+            </Link>
 
-        <div className="left-navbar-advertisting">
-          <i className="fa fa-star" aria-hidden="true"></i>
-          Explore!
+
+            <div className="left-navbar-explore">
+              <i className="fa fa-star" aria-hidden="true"></i>
+              <p>Explore</p>!
+            </div>
+          </div>
         </div>
-      </div>
-    );
+      );
+
+    } else {
+      return null;
+    }
+
   }
 }
 
-export default LeftBar;
+const mapStateToProps = state => ({
+  currentUser: state.session.currentUser,
+});
+
+export default connect(mapStateToProps, null)(LeftBar);
