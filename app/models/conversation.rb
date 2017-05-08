6638db90e62a6a9d0e9591fe407629e7e3538fa1
Akshith Yellapragada <ayellapragada@gmail.com>
2017-05-08 14:23:24 -0400

    Improve conversation and message models.

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: conversations
+#
+#  id           :integer          not null, primary key
+#  sender_id    :integer
+#  recipient_id :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Conversation < ActiveRecord::Base
   belongs_to :sender, :foreign_key => :sender_id, class_name: 'User'
   belongs_to :recipient, :foreign_key => :recipient_id, class_name: 'User'
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id              :integer          not null, primary key
+#  body            :text
+#  conversation_id :integer
+#  user_id         :integer
+#  read            :boolean          default(FALSE)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class Message < ActiveRecord::Base
   belongs_to :conversation
   belongs_to :user
