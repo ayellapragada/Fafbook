82dc29bbe138dc04e881567052241571293d0276
Akshith Yellapragada <ayellapragada@gmail.com>
2017-05-10 20:57:35 -0400

    Message input is taken and can be submitted, but now I need to use those calls to actually send that message to the database.

--- frontend/components/chat/open_chatbox_item.jsx
+++ frontend/components/chat/open_chatbox_item.jsx
@@ -5,12 +5,30 @@ import { Link } from 'react-router';
 class OpenChatboxItem extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {message: ""};
 
     this.handleCloseChat = this.handleCloseChat.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.checkSubmit = this.checkSubmit.bind(this);
   }
 
   handleCloseChat() {
     this.props.closeChat(this.props.chat);
+  }
+
+  handleChange(e) {
+    this.setState({message: e.currentTarget.value});
+  }
+
+  checkSubmit(e) {
+    if (e.keyCode === 13 && e.shiftKey === false) {
+      this.handleSubmit(e);
+    } 
+  }
+
+  handleSubmit(e) {
+    this.setState({message: ""});
   }
 
   render() {
@@ -19,21 +37,26 @@ class OpenChatboxItem extends React.Component {
         <div className="chatbox-header" onClick={this.props.toggleOpen} >
           <div className="chatbox-header-name">
             <Link to={`/profile/${this.props.otherUser.id}`}>
-            {`${this.props.otherUser.fname} ${this.props.otherUser.lname}`}
-          </Link>
+              {`${this.props.otherUser.fname} ${this.props.otherUser.lname}`}
+            </Link>
           </div>
 
           <div className="chatbox-header-controls">
             <i onClick={this.handleCloseChat} 
-                className="fa fa-times" aria-hidden="true"></i>
+              className="fa fa-times" aria-hidden="true"></i>
           </div>
 
         </div>
 
         <Messages />
 
-        <div>
-          Send New Message
+        <div className="send-chat-message">
+          <textarea 
+            className="send-chat-textarea"
+            value={this.state.message}
+            placeholder="Type a message"
+            onKeyDown={this.checkSubmit} 
+            onChange={this.handleChange} />
         </div>
       </div>
     );
